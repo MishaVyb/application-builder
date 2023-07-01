4cbee3c8cdb1e4a3aa2c7089381ada26cd3e3a60
Misha Vybornyi <vbrn.mv@gmail.com>
2023-07-01 18:50:07 +0400

    remove profile

--- app/schemas/builds.py
+++ app/schemas/builds.py
@@ -1,18 +1,9 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Generator
-from app.core import settings
 
 from app.schemas.base import BaseSchema
 from app.schemas.tasks import TaskSchema
-
-
-if settings.DEBUG:
-    import line_profiler
-
-    profile = line_profiler.LineProfiler()
-else:
-    profile = lambda x: x
 
 if TYPE_CHECKING:
     from app.main import TasksStore
@@ -28,7 +19,6 @@ class BuildSchema(BaseSchema):
         """Tasks as `TaskSchema` instances generator factory."""
         return (store[task_name] for task_name in self.tasks)
 
-    @profile
     def resolve(self, store: TasksStore):
         """Resolve tasks dependencies. Return sorted list."""
         for task in self.tasks_generator(store):
